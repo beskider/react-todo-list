11f9ab589a786300976c83024f9e50ad487568a7
kit <kit@kit.com>
2020-05-17 17:33:20 +0200

    Updates

---
+++ precache-manifest.96a8e1b247a697110bcf2842cce72acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed7695475b30a4ff86098a2cd176e80",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "ef82489d287175223ffb",
+    "url": "/react-todo-list/static/css/main.f4fb63cd.chunk.css"
+  },
+  {
+    "revision": "1caebbd1077aba0af951",
+    "url": "/react-todo-list/static/js/2.b1265fb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.b1265fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef82489d287175223ffb",
+    "url": "/react-todo-list/static/js/main.1cba4713.chunk.js"
+  },
+  {
+    "revision": "3f55eb4d5430f52ab291",
+    "url": "/react-todo-list/static/js/runtime-main.c6e33228.js"
+  }
+]);
